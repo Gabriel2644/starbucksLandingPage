eb773466b00fda1a5742e182c5f8801fc49a5682
Gabriel2644 <74109971+Gabriel2644@users.noreply.github.com>
2021-05-02 20:52:37 +0300

    A beautiful coffee shop landing page

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const imgSlider = (pic) => {
+  document.querySelector(".starbucks").src = pic;
+};
+const changeCircleColor = (color) => {
+  document.querySelector(".circle").style.background = color;
+};
